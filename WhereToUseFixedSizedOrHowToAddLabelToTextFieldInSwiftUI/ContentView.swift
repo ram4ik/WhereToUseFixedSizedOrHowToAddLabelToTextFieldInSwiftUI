7404c8f6f1dd32d0171f2c1b19868844a2b82726
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-15 11:18:35 +0300

    init

--- WhereToUseFixedSizedOrHowToAddLabelToTextFieldInSwiftUI/ContentView.swift
+++ WhereToUseFixedSizedOrHowToAddLabelToTextFieldInSwiftUI/ContentView.swift
@@ -9,8 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text = ""
+    let label = ["Username", "Email", "Paassword"]
+    
+    @State var horizontal = true
+    @State var vertical = false
+    @State var padding1 = Edge.Set.leading
+    @State var padding2 = Edge.Set.horizontal
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    ForEach(label, id: \.self) { label in
+                        Text(label)
+                    }
+                    .frame(maxHeight: .infinity)
+                }
+                .border(Color.blue)
+                
+                VStack {
+                    ForEach(label, id: \.self) { label in
+                        TextField(label, text: self.$text)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                }
+                .border(Color.blue)
+            }
+            .padding(.horizontal)
+        .fixedSize(horizontal: false, vertical: true)
+        }
     }
 }
 
